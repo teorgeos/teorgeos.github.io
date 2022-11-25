3e710e08386b586986ad69985de721085c669943
Hyebin Seo <88023620+teorgeos@users.noreply.github.com>
2022-11-25 22:43:10 +0900

    Add files via upload
    p5js drawing but the cursor should be fixed soon...

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var canvas;
+var h1;
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    background(255);
+    h1 = createElement("h1", "Love");
+}
+
+function mousePressed()
+{
+createP("What we need is");
+}
+
+function draw()
+{
+    fill(random(100, 255), 20, 150);
+    ellipse(mouseX, mouseY, 20, 20);
+    h1.position(mouseX, mouseY);
+}
